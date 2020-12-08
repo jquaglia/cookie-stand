3066548b22f46bbf70004adc7c57e20280082bfe
stubtail42 <jqugali@gmail.com>
2020-12-07 16:46:46 -0800

    got the 1st object to calculate properly

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# cookie-stand
+# LAB - Number 6
+
+## Salmon Cookies
+
+I built an app with a direct and powerful purpose. My app does all of the things that it accomplishes well. Here  I describe the purpose and functionality so those that visit my README understand the app.
+
+### Author: Jason Quaglia
+
+### Collaborators
+
+* Anthony Hayward
+
+* Carly Dekock
+
+* Anthony Johnson
+
+* Glenn Clark
+
+### Links and Resources
+
+* [Math.random()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random)
+
+### Reflections and Comments
+
+* Consider including the answers to your daily journal and submission questions here
+
+* This is also a good place to reflect on the tools and resources used and learned
--- js/app.js
+++ js/app.js
@@ -1,15 +1,11 @@
 'use strict';
 
 
-function randomNumber(min, max) {
-  return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
-}
-
 // global variables
-var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm',];
 
-// 1st object - get it to work
 
+// 1st object
 var seattleStore = {
   name: 'Seattle',
   min: 23,
@@ -17,11 +13,20 @@ var seattleStore = {
   avg: 6.3,
   hourlySales: [],
   dailyTotal: 0,
-  getRandomNumber: function () {
+  getRandomCustomers: function () {
+    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
   },
+  // populate hourlySales array
   calculateHourlySales() {
-    console.log(this.getRandomNumber());
+    for (var i = 0; i < hours.length; i++) {
+      // var calcSales = this.getRandomCustomers() * this.avg;
+      var calcSalesRound = Math.ceil(this.getRandomCustomers() * this.avg);
+      // this.hourlySales[i] = calcSalesRound;
+      this.hourlySales.push(calcSalesRound)[i];
+      this.dailyTotal += calcSalesRound;
+    }
   },
+  // render my list with total at the end
   render: function () {
     this.calculateHourlySales();
     //do the thing
@@ -29,3 +34,6 @@ var seattleStore = {
     console.log('inside render method');
   }
 };
+
+seattleStore.render();
+// console.log(seattleStore.hourlySales);
